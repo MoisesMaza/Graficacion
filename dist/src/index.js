0943adfc8c002ffcbe043352f1a107d8c0deed30
Moisés Maza <moises.m.aries10@gmail.com>
2023-01-16 21:12:39 -0600

    Se hicieron los ajustes del limite de las puertas.
    Y las dobles caras de las puertas y cajones.

--- dist/src/index.js
+++ dist/src/index.js
@@ -11,6 +11,16 @@ graphics = canvas.getContext('2d');
 var cv;
 var obj;
 var ang = 0;
+var contp1 = 0;
+var contp2 = 0;
+var limitep1 = 9;
+var limitep2 = 9;
+var limitec1 = 5;
+var limitec2 = 5;
+var limitec3 = 5;
+var contc1 = 0;
+var contc2 = 0;
+var contc3 = 0;
 function leerArchivo(e) {
     var archivo = e.target.files[0];
     if (!archivo) {
@@ -65,205 +75,265 @@ function decrDistFunc() {
 }
 // Cerrar puerta 1
 function pza1DerFunc() {
-    var af = 10;
-    Rota3D.initRotate(obj.w[130], obj.w[131], af * Math.PI / 180);
-    for (var i = 132; i <= 133; i++) {
-        obj.w[i] = Rota3D.rotate(obj.w[i]);
+    if (contp1 == 0) {
+        alert('La puerta no se puede cerrar más');
     }
-    cv.setObj(obj);
-    cv.paint();
+    else {
+        var af = 10;
+        Rota3D.initRotate(obj.w[130], obj.w[131], af * Math.PI / 180);
+        for (var i = 132; i <= 133; i++) {
+            obj.w[i] = Rota3D.rotate(obj.w[i]);
+        }
+        contp1--;
+        cv.setObj(obj);
+        cv.paint();
+    }
 }
 // Abrir puerta 1
 function pza1IzqFunc() {
-    var af = -10;
-    Rota3D.initRotate(obj.w[130], obj.w[131], af * Math.PI / 180);
-    for (var i = 132; i <= 133; i++) {
-        obj.w[i] = Rota3D.rotate(obj.w[i]);
+    if (contp1 < limitep1) {
+        var af = -10;
+        Rota3D.initRotate(obj.w[130], obj.w[131], af * Math.PI / 180);
+        for (var i = 132; i <= 133; i++) {
+            obj.w[i] = Rota3D.rotate(obj.w[i]);
+        }
+        contp1++;
+        cv.setObj(obj);
+        cv.paint();
     }
-    cv.setObj(obj);
-    cv.paint();
+    else {
+        alert('La puerta no se puede abrir más');
+    }
 }
 // Abrir puerta 2
 function pza12DerFunc() {
-    var af = 10;
-    Rota3D.initRotate(obj.w[151], obj.w[152], af * Math.PI / 180);
-    for (var i = 150; i <= 153; i++) {
-        obj.w[i] = Rota3D.rotate(obj.w[i]);
+    if (contp2 < limitep2) {
+        var af = 10;
+        Rota3D.initRotate(obj.w[151], obj.w[152], af * Math.PI / 180);
+        for (var i = 150; i <= 153; i++) {
+            obj.w[i] = Rota3D.rotate(obj.w[i]);
+        }
+        contp2++;
+        cv.setObj(obj);
+        cv.paint();
     }
-    cv.setObj(obj);
-    cv.paint();
+    else {
+        alert('La puerta no se puede abrir más');
+    }
 }
 //Cerrar puerta 2
 function pza12IzqFunc() {
-    var af = -10;
-    Rota3D.initRotate(obj.w[151], obj.w[152], af * Math.PI / 180);
-    for (var i = 150; i <= 153; i++) {
-        obj.w[i] = Rota3D.rotate(obj.w[i]);
+    if (contp2 == 0) {
+        alert('La puerta no se puede cerrar más');
     }
-    cv.setObj(obj);
-    cv.paint();
+    else {
+        var af = -10;
+        Rota3D.initRotate(obj.w[151], obj.w[152], af * Math.PI / 180);
+        for (var i = 150; i <= 153; i++) {
+            obj.w[i] = Rota3D.rotate(obj.w[i]);
+        }
+        contp2--;
+        cv.setObj(obj);
+        cv.paint();
+    }
 }
 //Abrir cajon 1
 function trayecto() {
-    var tr = 1;
-    for (var i = 230; i <= 231; i++) {
-        obj.w[i].z = obj.w[i].z + tr;
+    if (contc1 < limitec1) {
+        var tr = 0.5;
+        for (var i = 230; i <= 231; i++) {
+            obj.w[i].z = obj.w[i].z + tr;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr2 = 0.5;
+        for (var i = 232; i <= 233; i++) {
+            obj.w[i].z = obj.w[i].z + tr2;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr3 = 0.5;
+        for (var i = 234; i <= 235; i++) {
+            obj.w[i].z = obj.w[i].z + tr3;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr4 = 0.5;
+        for (var i = 236; i <= 237; i++) {
+            obj.w[i].z = obj.w[i].z + tr4;
+        }
+        contc1++;
+        cv.setObj(obj);
+        cv.paint();
     }
-    cv.setObj(obj);
-    cv.paint();
-    var tr2 = 1;
-    for (var i = 232; i <= 233; i++) {
-        obj.w[i].z = obj.w[i].z + tr2;
+    else {
+        alert('El cajón no se puede abrir más');
     }
-    cv.setObj(obj);
-    cv.paint();
-    var tr3 = 1;
-    for (var i = 234; i <= 235; i++) {
-        obj.w[i].z = obj.w[i].z + tr3;
-    }
-    cv.setObj(obj);
-    cv.paint();
-    var tr4 = 1;
-    for (var i = 236; i <= 237; i++) {
-        obj.w[i].z = obj.w[i].z + tr4;
-    }
-    cv.setObj(obj);
-    cv.paint();
 }
 //Cerrar cajon 1
 function trayectocerrar() {
-    var tr = -1;
-    for (var i = 230; i <= 231; i++) {
-        obj.w[i].z = obj.w[i].z + tr;
+    if (contc1 == 0) {
+        alert('El cajón no se puede cerrar más');
     }
-    cv.setObj(obj);
-    cv.paint();
-    var tr2 = -1;
-    for (var i = 232; i <= 233; i++) {
-        obj.w[i].z = obj.w[i].z + tr2;
+    else {
+        var tr = -0.5;
+        for (var i = 230; i <= 231; i++) {
+            obj.w[i].z = obj.w[i].z + tr;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr2 = -0.5;
+        for (var i = 232; i <= 233; i++) {
+            obj.w[i].z = obj.w[i].z + tr2;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr3 = -0.5;
+        for (var i = 234; i <= 235; i++) {
+            obj.w[i].z = obj.w[i].z + tr3;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr4 = -0.5;
+        for (var i = 236; i <= 237; i++) {
+            obj.w[i].z = obj.w[i].z + tr4;
+        }
+        contc1--;
+        cv.setObj(obj);
+        cv.paint();
     }
-    cv.setObj(obj);
-    cv.paint();
-    var tr3 = -1;
-    for (var i = 234; i <= 235; i++) {
-        obj.w[i].z = obj.w[i].z + tr3;
-    }
-    cv.setObj(obj);
-    cv.paint();
-    var tr4 = -1;
-    for (var i = 236; i <= 237; i++) {
-        obj.w[i].z = obj.w[i].z + tr4;
-    }
-    cv.setObj(obj);
-    cv.paint();
 }
 //Abrir cajon 2
 function trayecto2() {
-    var tr = 1;
-    for (var i = 260; i <= 261; i++) {
-        obj.w[i].z = obj.w[i].z + tr;
+    if (contc2 < limitec2) {
+        var tr = 0.5;
+        for (var i = 260; i <= 261; i++) {
+            obj.w[i].z = obj.w[i].z + tr;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr2 = 0.5;
+        for (var i = 262; i <= 263; i++) {
+            obj.w[i].z = obj.w[i].z + tr2;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr3 = 0.5;
+        for (var i = 264; i <= 265; i++) {
+            obj.w[i].z = obj.w[i].z + tr3;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr4 = 0.5;
+        for (var i = 266; i <= 267; i++) {
+            obj.w[i].z = obj.w[i].z + tr4;
+        }
+        contc2++;
+        cv.setObj(obj);
+        cv.paint();
     }
-    cv.setObj(obj);
-    cv.paint();
-    var tr2 = 1;
-    for (var i = 262; i <= 263; i++) {
-        obj.w[i].z = obj.w[i].z + tr2;
+    else {
+        alert('El cajón no se puede abrir más');
     }
-    cv.setObj(obj);
-    cv.paint();
-    var tr3 = 1;
-    for (var i = 264; i <= 265; i++) {
-        obj.w[i].z = obj.w[i].z + tr3;
-    }
-    cv.setObj(obj);
-    cv.paint();
-    var tr4 = 1;
-    for (var i = 266; i <= 267; i++) {
-        obj.w[i].z = obj.w[i].z + tr4;
-    }
-    cv.setObj(obj);
-    cv.paint();
 }
 //Cerrar cajon 2
 function trayectocerrar2() {
-    var tr = -1;
-    for (var i = 260; i <= 261; i++) {
-        obj.w[i].z = obj.w[i].z + tr;
+    if (contc2 == 0) {
+        alert('El cajón no se puede cerrar más');
     }
-    cv.setObj(obj);
-    cv.paint();
-    var tr2 = -1;
-    for (var i = 262; i <= 263; i++) {
-        obj.w[i].z = obj.w[i].z + tr2;
+    else {
+        var tr = -0.5;
+        for (var i = 260; i <= 261; i++) {
+            obj.w[i].z = obj.w[i].z + tr;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr2 = -0.5;
+        for (var i = 262; i <= 263; i++) {
+            obj.w[i].z = obj.w[i].z + tr2;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr3 = -0.5;
+        for (var i = 264; i <= 265; i++) {
+            obj.w[i].z = obj.w[i].z + tr3;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr4 = -0.5;
+        for (var i = 266; i <= 267; i++) {
+            obj.w[i].z = obj.w[i].z + tr4;
+        }
+        contc2--;
+        cv.setObj(obj);
+        cv.paint();
     }
-    cv.setObj(obj);
-    cv.paint();
-    var tr3 = -1;
-    for (var i = 264; i <= 265; i++) {
-        obj.w[i].z = obj.w[i].z + tr3;
-    }
-    cv.setObj(obj);
-    cv.paint();
-    var tr4 = -1;
-    for (var i = 266; i <= 267; i++) {
-        obj.w[i].z = obj.w[i].z + tr4;
-    }
-    cv.setObj(obj);
-    cv.paint();
 }
 //Abrir cajon 3
 function trayecto3() {
-    var tr = 1;
-    for (var i = 290; i <= 291; i++) {
-        obj.w[i].z = obj.w[i].z + tr;
+    if (contc3 < limitec3) {
+        var tr = 0.5;
+        for (var i = 290; i <= 291; i++) {
+            obj.w[i].z = obj.w[i].z + tr;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr2 = 0.5;
+        for (var i = 292; i <= 293; i++) {
+            obj.w[i].z = obj.w[i].z + tr2;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr3 = 0.5;
+        for (var i = 294; i <= 295; i++) {
+            obj.w[i].z = obj.w[i].z + tr3;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr4 = 0.5;
+        for (var i = 296; i <= 297; i++) {
+            obj.w[i].z = obj.w[i].z + tr4;
+        }
+        contc3++;
+        cv.setObj(obj);
+        cv.paint();
     }
-    cv.setObj(obj);
-    cv.paint();
-    var tr2 = 1;
-    for (var i = 292; i <= 293; i++) {
-        obj.w[i].z = obj.w[i].z + tr2;
+    else {
+        alert('El cajón no se puede abrir más');
     }
-    cv.setObj(obj);
-    cv.paint();
-    var tr3 = 1;
-    for (var i = 294; i <= 295; i++) {
-        obj.w[i].z = obj.w[i].z + tr3;
-    }
-    cv.setObj(obj);
-    cv.paint();
-    var tr4 = 1;
-    for (var i = 296; i <= 297; i++) {
-        obj.w[i].z = obj.w[i].z + tr4;
-    }
-    cv.setObj(obj);
-    cv.paint();
 }
 //Cerrar cajon 3
 function trayectocerrar3() {
-    var tr = -1;
-    for (var i = 290; i <= 291; i++) {
-        obj.w[i].z = obj.w[i].z + tr;
+    if (contc3 == 0) {
+        alert('El cajón no se puede cerrar más');
     }
-    cv.setObj(obj);
-    cv.paint();
-    var tr2 = -1;
-    for (var i = 292; i <= 293; i++) {
-        obj.w[i].z = obj.w[i].z + tr2;
+    else {
+        var tr = -0.5;
+        for (var i = 290; i <= 291; i++) {
+            obj.w[i].z = obj.w[i].z + tr;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr2 = -0.5;
+        for (var i = 292; i <= 293; i++) {
+            obj.w[i].z = obj.w[i].z + tr2;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr3 = -0.5;
+        for (var i = 294; i <= 295; i++) {
+            obj.w[i].z = obj.w[i].z + tr3;
+        }
+        cv.setObj(obj);
+        cv.paint();
+        var tr4 = -0.5;
+        for (var i = 296; i <= 297; i++) {
+            obj.w[i].z = obj.w[i].z + tr4;
+        }
+        contc3--;
+        cv.setObj(obj);
+        cv.paint();
     }
-    cv.setObj(obj);
-    cv.paint();
-    var tr3 = -1;
-    for (var i = 294; i <= 295; i++) {
-        obj.w[i].z = obj.w[i].z + tr3;
-    }
-    cv.setObj(obj);
-    cv.paint();
-    var tr4 = -1;
-    for (var i = 296; i <= 297; i++) {
-        obj.w[i].z = obj.w[i].z + tr4;
-    }
-    cv.setObj(obj);
-    cv.paint();
 }
 document.getElementById('file-input').addEventListener('change', leerArchivo, false);
 document.getElementById('eyeDown').addEventListener('click', eyeDownFunc, false);
--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,17 @@ graphics = canvas.getContext('2d');
 let cv: CvHLines;
 let obj: Obj3D;
 let ang: number=0;
+let contp1: number =0;
+let contp2: number =0;
+let limitep1: number = 9;
+let limitep2: number = 9;
+let limitec1: number = 5;
+let limitec2: number = 5;
+let limitec3: number = 5;
+let contc1: number = 0;
+let contc2: number = 0;
+let contc3: number = 0;
+
 
 function leerArchivo(e:any) {
   var archivo = e.target.files[0];
@@ -79,236 +90,284 @@ function decrDistFunc() {
 }
 // Cerrar puerta 1
 function pza1DerFunc() {
-  let af = 10;
- 	
-	Rota3D.initRotate( obj.w[130], obj.w[131], af*Math.PI/180);	
-	
-  for (let i = 132; i <= 133; i++){
-    obj.w[i] = Rota3D.rotate(obj.w[i]);
-	}
-	cv.setObj(obj);
-  cv.paint();	
+  if (contp1 == 0) {
+    alert('La puerta no se puede cerrar más'); }
+    else{
+    let af = 10;
+    Rota3D.initRotate( obj.w[130], obj.w[131], af*Math.PI/180);	
+    for (let i = 132; i <= 133; i++){
+      obj.w[i] = Rota3D.rotate(obj.w[i]);
+    }
+    contp1--;
+    cv.setObj(obj);
+    cv.paint();	
+  }
 }
 // Abrir puerta 1
 function pza1IzqFunc() {
-  let af = -10;
- 	
-	Rota3D.initRotate( obj.w[130], obj.w[131], af*Math.PI/180);	
-	
-  for (let i = 132; i <= 133; i++){
-    obj.w[i] = Rota3D.rotate(obj.w[i]);
-	}
-	cv.setObj(obj);
-  cv.paint();	
+  if (contp1 < limitep1) {
+    let af = -10;
+    Rota3D.initRotate( obj.w[130], obj.w[131], af*Math.PI/180);	
+    for (let i = 132; i <= 133; i++){
+      obj.w[i] = Rota3D.rotate(obj.w[i]);
+    }
+    contp1++;
+    cv.setObj(obj);
+    cv.paint();	
+  } else {
+    alert('La puerta no se puede abrir más');
+  }
 }
 // Abrir puerta 2
 function pza12DerFunc() {
-  let af = 10;
- 	
-	Rota3D.initRotate( obj.w[151], obj.w[152], af*Math.PI/180);	
-	
-  for (let i = 150; i <= 153; i++){
-    obj.w[i] = Rota3D.rotate(obj.w[i]);
-	}
-	cv.setObj(obj);
-  cv.paint();	
+  if (contp2 < limitep2) {
+    let af = 10;
+    Rota3D.initRotate( obj.w[151], obj.w[152], af*Math.PI/180);	
+    for (let i = 150; i <= 153; i++){
+      obj.w[i] = Rota3D.rotate(obj.w[i]);
+    }
+    contp2++;
+    cv.setObj(obj);
+    cv.paint();	
+  } else {
+    alert('La puerta no se puede abrir más');
+  }
 }
 //Cerrar puerta 2
 function pza12IzqFunc() {
-  let af = -10;
- 	
-	Rota3D.initRotate( obj.w[151], obj.w[152], af*Math.PI/180);	
-	
-  for (let i = 150; i <= 153; i++){
-    obj.w[i] = Rota3D.rotate(obj.w[i]);
-	}
-	cv.setObj(obj);
-  cv.paint();	
+  if (contp2 == 0) {
+    alert('La puerta no se puede cerrar más'); }
+    else {
+    let af = -10;
+    Rota3D.initRotate( obj.w[151], obj.w[152], af*Math.PI/180);	
+    for (let i = 150; i <= 153; i++){
+      obj.w[i] = Rota3D.rotate(obj.w[i]);
+    }
+    contp2--;
+    cv.setObj(obj);
+    cv.paint();	
+  }
 }
 //Abrir cajon 1
 function trayecto(){
-  var tr = 1;
+  if(contc1 < limitec1) {
+  var tr = 0.5;
   for(var i=230; i<=231; i++){
     obj.w[i].z= obj.w[i].z+tr;
   }
   cv.setObj(obj);
   cv.paint();
   
-  var tr2 = 1;
+  var tr2 = 0.5;
   for(var i=232; i<=233; i++){
     obj.w[i].z= obj.w[i].z+tr2;
   }
   cv.setObj(obj);
   cv.paint();
 
-  var tr3 = 1;
+  var tr3 = 0.5;
   for(var i=234; i<=235; i++){
     obj.w[i].z= obj.w[i].z+tr3;
   }
   cv.setObj(obj);
   cv.paint();
 
-  var tr4 = 1;
+  var tr4 = 0.5;
   for(var i=236; i<=237; i++){
     obj.w[i].z= obj.w[i].z+tr4;
   }
+  contc1++;
   cv.setObj(obj);
   cv.paint();
+}
+else {
+  alert('El cajón no se puede abrir más');
+}
 }
 
 //Cerrar cajon 1
 function trayectocerrar(){
-  var tr = -1;
+  if(contc1 == 0) {
+    alert('El cajón no se puede cerrar más');
+  }
+  else {
+  var tr = -0.5;
   for(var i=230; i<=231; i++){
     obj.w[i].z= obj.w[i].z+tr;
   }
   cv.setObj(obj);
   cv.paint();
   
-  var tr2 = -1;
+  var tr2 = -0.5;
   for(var i=232; i<=233; i++){
     obj.w[i].z= obj.w[i].z+tr2;
   }
   cv.setObj(obj);
   cv.paint();
 
-  var tr3 = -1;
+  var tr3 = -0.5;
   for(var i=234; i<=235; i++){
     obj.w[i].z= obj.w[i].z+tr3;
   }
   cv.setObj(obj);
   cv.paint();
 
-  var tr4 = -1;
+  var tr4 = -0.5;
   for(var i=236; i<=237; i++){
     obj.w[i].z= obj.w[i].z+tr4;
   }
+  contc1--;
   cv.setObj(obj);
   cv.paint();
+}
 }
 
 //Abrir cajon 2
 function trayecto2(){
-  var tr = 1;
+  if(contc2 < limitec2){
+  var tr = 0.5;
   for(var i=260; i<=261; i++){
     obj.w[i].z= obj.w[i].z+tr;
   }
   cv.setObj(obj);
   cv.paint();
   
-  var tr2 = 1;
+  var tr2 = 0.5;
   for(var i=262; i<=263; i++){
     obj.w[i].z= obj.w[i].z+tr2;
   }
   cv.setObj(obj);
   cv.paint();
 
-  var tr3 = 1;
+  var tr3 = 0.5;
   for(var i=264; i<=265; i++){
     obj.w[i].z= obj.w[i].z+tr3;
   }
   cv.setObj(obj);
   cv.paint();
 
-  var tr4 = 1;
+  var tr4 = 0.5;
   for(var i=266; i<=267; i++){
     obj.w[i].z= obj.w[i].z+tr4;
   }
+  contc2++;
   cv.setObj(obj);
   cv.paint();
+}
+  else{
+    alert('El cajón no se puede abrir más');
+  }
 }
 
 //Cerrar cajon 2
 function trayectocerrar2(){
-  var tr = -1;
+  if(contc2 == 0){
+    alert('El cajón no se puede cerrar más');
+  }
+  else {
+  var tr = -0.5;
   for(var i=260; i<=261; i++){
     obj.w[i].z= obj.w[i].z+tr;
   }
   cv.setObj(obj);
   cv.paint();
   
-  var tr2 = -1;
+  var tr2 = -0.5;
   for(var i=262; i<=263; i++){
     obj.w[i].z= obj.w[i].z+tr2;
   }
   cv.setObj(obj);
   cv.paint();
 
-  var tr3 = -1;
+  var tr3 = -0.5;
   for(var i=264; i<=265; i++){
     obj.w[i].z= obj.w[i].z+tr3;
   }
   cv.setObj(obj);
   cv.paint();
 
-  var tr4 = -1;
+  var tr4 = -0.5;
   for(var i=266; i<=267; i++){
     obj.w[i].z= obj.w[i].z+tr4;
   }
+  contc2--;
   cv.setObj(obj);
   cv.paint();
+}
 }
 
 //Abrir cajon 3
 function trayecto3(){
-  var tr = 1;
+  if(contc3 < limitec3){
+  var tr = 0.5;
   for(var i=290; i<=291; i++){
     obj.w[i].z= obj.w[i].z+tr;
   }
   cv.setObj(obj);
   cv.paint();
   
-  var tr2 = 1;
+  var tr2 = 0.5;
   for(var i=292; i<=293; i++){
     obj.w[i].z= obj.w[i].z+tr2;
   }
   cv.setObj(obj);
   cv.paint();
 
-  var tr3 = 1;
+  var tr3 = 0.5;
   for(var i=294; i<=295; i++){
     obj.w[i].z= obj.w[i].z+tr3;
   }
   cv.setObj(obj);
   cv.paint();
 
-  var tr4 = 1;
+  var tr4 = 0.5;
   for(var i=296; i<=297; i++){
     obj.w[i].z= obj.w[i].z+tr4;
   }
+  contc3++;
   cv.setObj(obj);
   cv.paint();
+}
+else {
+  alert('El cajón no se puede abrir más');
+}
 }
 
 //Cerrar cajon 3
 function trayectocerrar3(){
-  var tr = -1;
+  if(contc3 ==0) {
+    alert('El cajón no se puede cerrar más');
+  }
+  else {
+  var tr = -0.5;
   for(var i=290; i<=291; i++){
     obj.w[i].z= obj.w[i].z+tr;
   }
   cv.setObj(obj);
   cv.paint();
   
-  var tr2 = -1;
+  var tr2 = -0.5;
   for(var i=292; i<=293; i++){
     obj.w[i].z= obj.w[i].z+tr2;
   }
   cv.setObj(obj);
   cv.paint();
 
-  var tr3 = -1;
+  var tr3 = -0.5;
   for(var i=294; i<=295; i++){
     obj.w[i].z= obj.w[i].z+tr3;
   }
   cv.setObj(obj);
   cv.paint();
 
-  var tr4 = -1;
+  var tr4 = -0.5;
   for(var i=296; i<=297; i++){
     obj.w[i].z= obj.w[i].z+tr4;
   }
+  contc3--;
   cv.setObj(obj);
   cv.paint();
+}
 }
 
 document.getElementById('file-input').addEventListener('change', leerArchivo, false);
